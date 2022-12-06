1bbdbae23c8add8476bd750572fe460d53231139
KelanMorgan0 <c00262090@itcarlow.ie>
2022-12-06 11:03:48 +0000

    upload lab 10

---
+++ Labs/src/ie/setu/Lab10/ChangeColourDriver.java
@@ -0,0 +1,16 @@
+package ie.setu.Lab10;
+
+import javax.swing.JFrame;
+
+public class ChangeColourDriver {
+
+	public static void main(String[] args) {
+	
+		ChangeColour changeColour = new ChangeColour("Button");
+		changeColour.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		changeColour.setSize(200, 200);
+		changeColour.setVisible(true);
+
+	}//end main
+
+}//end class
