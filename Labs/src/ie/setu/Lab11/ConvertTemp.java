038e6eb75f45f6757bee108dc7b0aecefdfe0b57
KelanMorgan0 <c00262090@itcarlow.ie>
2023-01-10 10:12:34 +0000

    upload Lab11

---
+++ Labs/src/ie/setu/Lab11/ConvertTemp.java
@@ -0,0 +1,58 @@
+package ie.setu.Lab11;
+
+import java.awt.Desktop.Action;
+
+//Converts a Fahrenheit temperature entered by the user to Celsius, or vice versa
+
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class ConvertTemp extends JFrame {
+	
+	private JTextField fahrenField = new JTextField();
+	private JTextField celsiusField = new JTextField();
+	
+	// Constructor
+	public ConvertTemp(String title)	{
+		// Set title for frame and choose layout
+		super(title);
+		getContentPane().setLayout(new GridLayout(2, 2));
+
+		// Add Fahrenheit label and text field to frame
+		add(new JLabel("Fahrenheit"));
+		add(fahrenField);
+		fahrenField.addActionListener(new Convert());
+		// Attach listener to text field
+
+		// Add Celsius label and text field to frame
+		add(new JLabel("Celsius"));
+		add(celsiusField);
+		celsiusField.setEditable(false);
+		// Attach listener to text field
+
+		// Attach window listener
+		addWindowListener(new WindowCloser());
+	}//end costructor
+	
+	// Listener for window
+	class WindowCloser extends WindowAdapter {
+		public void windowClosing(WindowEvent evt) {
+			System.exit(0);
+		}
+	}
+	
+	class Convert implements ActionListener {
+		public void actionPerformed(ActionEvent evt) {
+			double celsius = (Double.parseDouble(fahrenField.getText())-32)*5/9;
+			celsiusField.setText(Double.toString(Math.round(celsius*100.0)/100.0));
+		}
+
+	}
+
+}//end class
